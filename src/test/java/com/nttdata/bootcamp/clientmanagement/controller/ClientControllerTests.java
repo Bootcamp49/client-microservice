8c9792dfc0bd70b59a6dd05e954b6dd113ee5bc5
Flavio Castillo <flavio.castillo@hotmail.com>
2024-01-27 20:10:34 -0500

    Added first tests to Controller

---
+++ src/test/java/com/nttdata/bootcamp/clientmanagement/controller/ClientControllerTests.java
@@ -0,0 +1,110 @@
+package com.nttdata.bootcamp.clientmanagement.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.nttdata.bootcamp.clientmanagement.model.Client;
+import com.nttdata.bootcamp.clientmanagement.service.ClientService;
+import com.nttdata.bootcamp.clientmanagement.util.ConstantsUtil;
+import com.nttdata.bootcamp.clientmanagement.util.Util;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@SpringBootTest
+class ClientControllerTests {
+
+    @Autowired
+    private ClientController clientController;
+
+    @MockBean
+    private ClientService clientService;
+
+    private Util util = new Util();
+
+    @Test
+    void contextLoads() {
+        assertThat(clientController).isNotNull();
+    }
+
+    @Test
+    void testFindClients() {
+        when(clientService.findAllClients()).thenReturn(Flux.just(clientMockResponse()));
+        Flux<Client> responseFlux = clientController.findClients();
+        Flux<Client> responseToCompare = Flux.just(
+                util.serializeArchive(ConstantsUtil.findAllClientsMock_Success, Client[].class));
+        assertThat(responseFlux).usingRecursiveComparison().isEqualTo(responseToCompare);
+    }
+
+    @Test
+    void testCreateClient() {
+        when(clientService.createClient(any(Client.class)))
+                .thenReturn(Mono.just(clientMockResponse()));
+        Client mockedRequest = clientMockRequest();
+        Mono<Client> responseClientController = clientController.createClient(mockedRequest);
+        Mono<Client> responseToCompare = Mono.just(
+            util.serializeArchive(ConstantsUtil.createClientMock_Success, Client.class)
+        );
+        assertThat(responseClientController)
+        .usingRecursiveComparison().isEqualTo(responseToCompare);
+    }
+
+    @Test
+    void testDeleteClient() {
+        when(clientService.deleteClient(anyString()))
+                .thenReturn(Mono.empty());
+        clientController.deleteClient("1234");
+        verify(clientService).deleteClient(anyString());
+    }
+
+    @Test
+    void testFindById() {
+        when(clientService.findById(anyString())).thenReturn(Mono.just(clientMockResponse()));
+        Mono<Client> responseMono = clientController.findById("asd1234567890fghjkl");
+        Mono<Client> responseToCompare = Mono.just(
+            util.serializeArchive(ConstantsUtil.findByIdClientMock_Success, Client.class));
+        assertThat(responseMono)
+        .usingRecursiveComparison().isEqualTo(responseToCompare);
+    }
+
+    @Test
+    void testUpdateClient() {
+        when(clientService.updateClient(anyString(), any(Client.class)))
+                .thenReturn(Mono.just(clientMockResponse()));
+        Client mockedRequest = clientMockRequest();
+        Mono<Client> responseClientController = 
+            clientController.updateClient("asd1234567890fghjkl", mockedRequest);
+        Mono<Client> responseToCompare = Mono.just(
+            util.serializeArchive(ConstantsUtil.updateClientMock_Success, Client.class)
+        );
+        assertThat(responseClientController)
+        .usingRecursiveComparison().isEqualTo(responseToCompare);
+    }
+
+    private Client clientMockResponse() {
+        Client clientMocked = new Client();
+        clientMocked.setId("asd1234567890fghjkl");
+        clientMocked.setClientType("personal");
+        clientMocked.setDocumentNumber("72094565");
+        clientMocked.setLastName("Ramos");
+        clientMocked.setName("Juan");
+        clientMocked.setPassword("12345");
+        return clientMocked;
+    }
+
+    private Client clientMockRequest() {
+        Client clientMocked = new Client();
+        clientMocked.setClientType("personal");
+        clientMocked.setDocumentNumber("72094565");
+        clientMocked.setLastName("Ramos");
+        clientMocked.setName("Juan");
+        clientMocked.setPassword("12345");
+        return clientMocked;
+    }
+}
---
+++ src/test/java/com/nttdata/bootcamp/clientmanagement/util/ConstantsUtil.java
@@ -0,0 +1,15 @@
+package com.nttdata.bootcamp.clientmanagement.util;
+
+/**
+ * Clase para almacenar constates por defecto.
+ */
+public class ConstantsUtil {
+    public static final String defaulEncodingApi = "UTF-8";
+
+    //#region Constantes para archivos json
+    public static final String findAllClientsMock_Success = "ClientsJson/FindAllClientsMock.json";
+    public static final String createClientMock_Success = "ClientsJson/CreateClientMock.json";
+    public static final String findByIdClientMock_Success = "ClientsJson/FindByIdClientMock.json";
+    public static final String updateClientMock_Success = "ClientsJson/UpdateClientMock.json";
+    //#endregion
+}
