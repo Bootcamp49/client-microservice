90d0a369e8b4af19329f45cf2cf3a0009fcee776
Flavio Castillo <flavio.castillo@hotmail.com>
2024-02-08 23:42:37 -0500

    Added library and configuration to enable discoveryClient

--- src/main/java/com/nttdata/bootcamp/clientmanagement/ClientApplication.java
+++ src/main/java/com/nttdata/bootcamp/clientmanagement/ClientApplication.java
@@ -2,10 +2,12 @@ package com.nttdata.bootcamp.clientmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Clase principal para la ejecución del microservicio.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ClientApplication {
 
